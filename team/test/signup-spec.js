adffb71ce98445a4c24d6c5a57e344ebf3f861d7
cindy nguyen <cnguyen0@uw.edu>
2015-11-14 17:42:20 -0800

    finished last name. please check

--- team/test/signup-spec.js
+++ team/test/signup-spec.js
@@ -3,17 +3,14 @@ describe('the form app', function() {
 	var requiredMsg = $('.bDate-required-error');
 	var vDateMsg = $('.vDate-required-error');
 	var vAgeMsg = $('.vAge-required-error');
+    var requiredName = $('.lNameInp-error');
 
 	var bDateImp = element(by.model('user.bDate'));
+    var lnameInp = element(by.model('user.lname'));
 
     beforeEach(function() { // run this function before each test
         browser.get('http://localhost:8000/');
     });
-
-    // Cee will work on later. Have question.
-    /*it('must have a last name', function() {
-        expect(browser.getTitle()).toEqual('My Tasks');
-    });*/
 
 	it('must show required validation error', function(){
         // isPresent(): get a boolean to decide whether the msg is there for not
@@ -21,15 +18,6 @@ describe('the form app', function() {
 	beforeEach(function() {
         browser.get('http://localhost:8000');
     })
-
-    it('must show required validation error', function(){
-        expect(requiredMsg.isPresent()).toEqual(false);
-        bDateImp.sendKeys('abc');
-        bDateImp.clear();
-        expect(requiredMsg.isPresent()).toEqual(true);
-        bDateImp.sendKeys('abc');
-        expect(requiredMsg.isPresent()).toEqual(false);
-    });
 
     it('must show valid date validation error', function() {
         expect(vDateMsg.isPresent()).toEqual(false);
@@ -39,6 +27,13 @@ describe('the form app', function() {
         expect(requiredMsg.isPresent()).toEqual(true);
         bDateImp.sendKeys('abc');
         expect(vDateMsg.isPresent()).toEqual(true);
+
+        expect(requiredName.isPresent()).toEqual(false); //isPresent gets boolean
+        taskTitleInp.sendKeys('abc');
+        taskTitleInp.clear(); //clear method to clear input
+        expect(requiredName.isPresent()).toEqual(true);
+        taskTitleInp.sendKeys('abc');
+        expect(requiredName.isPresent()).toEqual(false);
     })
    
     it('must show valid age validation error', function() {
